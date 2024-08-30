97a71bb67225aa9ca64b5f434b3b018f6ee77f1d
Alexey Lukyanov <alexey.a.lukyanov@gmail.com>
2024-08-30 09:51:19 -0500

    Add test script that can submit email

---
+++ scripts/python/email_send_example.py
@@ -0,0 +1,98 @@
+import os
+import boto3
+
+from dotenv import load_dotenv
+from botocore.exceptions import ClientError
+
+
+"""
+This is an example of how an email can be sent. I am going to work on that email 
+template until I reach something really useful. 
+
+The following env vars must be defined in the .env file:
+
+"""
+
+
+load_dotenv()
+
+
+# Replace sender@example.com with your "From" address.
+# This address must be verified with Amazon SES.
+SENDER = f"Alexey Lukyanov <{os.environ.get('SES_EMAIL')}>"
+
+# This address must be verified until the account is in the sandbox
+RECIPIENT = os.environ.get('SES_VERIFIED_RECIPIENT')
+
+# Specify a configuration set. If you do not want to use a configuration
+# set, comment the following variable, and the
+# ConfigurationSetName=CONFIGURATION_SET argument below.
+# CONFIGURATION_SET = "ConfigSet"
+
+#
+AWS_REGION = "us-east-1"
+
+# The subject line for the email.
+SUBJECT = "Amazon SES Test (SDK for Python)"
+
+# The email body for recipients with non-HTML email clients.
+BODY_TEXT = ("Amazon SES Test (Python)\r\n"
+             "This email was sent with Amazon SES using the "
+             "AWS SDK for Python (Boto)."
+             )
+
+# The HTML body of the email.
+BODY_HTML = """<html>
+<head></head>
+<body>
+  <h1>Amazon SES Test (SDK for Python)</h1>
+  <p>This email was sent with
+    <a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the
+    <a href='https://aws.amazon.com/sdk-for-python/'>
+      AWS SDK for Python (Boto)</a>.</p>
+</body>
+</html>
+            """
+
+# The character encoding for the email.
+CHARSET = "UTF-8"
+
+# Create a new SES resource and specify a region.
+client = boto3.client('ses', region_name=AWS_REGION)
+
+# Try to send the email.
+try:
+    # Provide the contents of the email.
+    response = client.send_email(
+        Destination={
+            'ToAddresses': [
+                RECIPIENT,
+            ],
+        },
+        Message={
+            'Body': {
+                'Html': {
+                    'Charset': CHARSET,
+                    'Data': BODY_HTML,
+                },
+                'Text': {
+                    'Charset': CHARSET,
+                    'Data': BODY_TEXT,
+                },
+            },
+            'Subject': {
+                'Charset': CHARSET,
+                'Data': SUBJECT,
+            },
+        },
+        Source=SENDER,
+        # If you are not using a configuration set, comment or delete the
+        # following line
+        # ConfigurationSetName=CONFIGURATION_SET,
+    )
+# Display an error if something goes wrong.
+except ClientError as e:
+    print(e.response['Error']['Message'])
+else:
+    print("Email sent! Message ID:"),
+    print(response['MessageId'])
